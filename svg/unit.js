038aa57de9d6e1af06c278b92b1fa60f056f1ca5
pikpokjeon <pikpokjeon@gmail.com>
2021-09-11 22:19:57 +0900

    feature unit calc function

---
+++ svg/unit.js
@@ -0,0 +1,38 @@
+export const getUnit = (w, h, data) =>
+{
+    const unitX = w / data.length
+    const gap = unitX / data.length
+    const [height, margin] = [350, -50]
+    const [maxData, minData] = [
+        Math.max(...Array.from(data)),
+        Math.min(...Array.from(data)),
+    ]
+    const MAX = Math.max(maxData, Math.abs(minData))
+    const SUM = maxData + Math.abs(minData)
+    const unitY = height / MAX
+    return {
+        unit: {
+            x: unitX,
+            y: unitY,
+            gap,
+        },
+        size: {
+            w,
+            h,
+        },
+        data: {
+            MAX,
+            SUM,
+        },
+        chartData: {
+            maxData,
+            minData,
+        },
+        margin: {
+            chartMargin: -50,
+            left: 155,
+            chartLeft: margin,
+        },
+    }
+}
+
