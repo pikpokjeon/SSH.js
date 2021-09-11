1c613fe14652f6a5bdb6b2e43e004cc839b53e8f
pikpokjeon <pikpokjeon@gmail.com>
2021-09-11 22:23:38 +0900

    refactor 불필요한 함수 제거

--- svg/coordinate.js
+++ svg/coordinate.js
@@ -7,25 +7,7 @@ export const genCoordinate = (unitData) =>
     const x = (i) => Math.floor(unitData.unit.x * i)
     const y = (v) => unitData.chartMargin + (MAX - v) * unitData.unit.y
     const idx = (x) => Math.floor(x / (unitData.unit.x + unitData.unit.gap))
-    const group = {
-        x: (i) => ({
-            single: {x: x(i)},
-            couple: {
-                x1: x(i),
-                x2: x(i),
-            },
-        }),
-        y: (v) => ({
-            single: {
-                y: y(v),
-            },
-            couple: {
-                y1: y(v),
-                y2: y(v),
-            },
-        }),
-    }
-    return {x, y, idx, group}
+    return {x, y, idx}
 }
 
 export const coordinate = (w, d) => genCoordinate(getUnit(w, d))
